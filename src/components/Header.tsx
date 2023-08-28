80414e797d6007c5e3a193f885ba6813ccb68c66
zerK4 <sebastian.v.pavel@gmail.com>
2023-08-28 15:01:49 +0300

    update favicon and homepage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,14 +8,16 @@ function Header() {
   return (
     <div className="flex justify-center">
       <div className="controlledWidth flex justify-between items-center h-24">
-        <div className="relative w-20 h-20">
-          <Image
-            src="/assets/logo.png"
-            alt="Logo image"
-            fill
-            className="object-cover"
-          />
-        </div>
+        <Link href="/">
+          <div className="relative w-20 h-20">
+            <Image
+              src="/assets/logo.png"
+              alt="Logo image"
+              fill
+              className="object-cover"
+            />
+          </div>
+        </Link>
         <Navigation />
         <div className="flex md:hidden">
           <NavigationMobile />
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,29 +2,35 @@ import Image from "next/image";
 import React from "react";
 import { BsFacebook } from "react-icons/bs";
 import { AiFillInstagram, AiFillYoutube } from "react-icons/ai";
+import { Bricolage_Grotesque } from "next/font/google";
+import Link from "next/link";
+
+const bGro = Bricolage_Grotesque({ subsets: ["latin"] });
 
 function Hero() {
   return (
     <div className="w-full relative flex justify-center">
       <div className="controlledWidth relative">
-        <div className="relative w-full h-[60vh] lg:h-[90vh] z-10 md:translate-x-16">
+        <div className="relative w-full h-[60vh] md:h-[90vh] lg:h-[90vh] z-10 translate-x-12">
           <Image
             src="/assets/hero-image.png"
             fill
             priority
             alt="Hero image"
-            className="object-scale-down"
+            className="object-scale-down opacity-90"
           />
         </div>
-        <div className="absolute left-0 top-0 h-full flex items-center">
-          <div className="flex flex-col gap-2">
-            <BsFacebook className="text-3xl text-sky-600 hover:text-sky-800 ease-in-out duration-300" />
-            <AiFillInstagram className="text-3xl text-[rgb(184,16,118)] hover:text-[rgb(184,16,218)] ease-in-out duration-300" />
-            <AiFillYoutube className="text-3xl text-red-500 hover:text-red-700 ease-in-out duration-300" />
-          </div>
-          <div className="ml-14 z-20">
-            <h1 className="text-5xl text-[rgb(55,0,112)] font-bold max-w-[5rem]">
-              Magnitude dance
+        <div className="absolute left-0 top-20 md:top-60 h-full flex justify-center w-full">
+          <div className={``}>
+            <h1
+              className={`${bGro} text-[4rem] sm:text-[7rem] md:text-[8rem] lg:text-[10rem] xl:text-[15rem] text-[rgb(55,0,112)] leading-[100px]  font-bold flex flex-col w-full`}
+            >
+              <span
+                className={`${bGro} drop-shadow-[0_1.2px_1.2px_rgb(0,0,0,.5)]`}
+              >
+                Magnitude
+              </span>
+              <span className="z-20 w-full text-end">dance</span>
             </h1>
           </div>
         </div>
