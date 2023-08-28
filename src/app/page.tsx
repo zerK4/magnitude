c7d70b70246c4daf57272a1ff471f3824ba637e4
zerK4 <sebastian.v.pavel@gmail.com>
2023-08-28 15:18:40 +0300

    add trainer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,16 @@
 
 import Banner from "@/components/Banner";
 import Hero from "@/components/Hero";
+import TrainerComponent from "@/components/TrainerComponent";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="mb-40">
+    <div className="mb-40 overflow-x-hidden">
       <Hero />
       <Banner />
+      <TrainerComponent />
     </div>
   );
 }
---
+++ src/components/TrainerComponent.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import React from "react";
+import { Button } from "./ui/button";
+
+function TrainerComponent() {
+  return (
+    <div className="flex justify-center min-h-[40vh] md:mt-32 overflow-hidden">
+      <div className="controlledWidth">
+        <h2 className="text-mag text-5xl font-bold">Instructor</h2>
+        <div className="mt-4 flex items-center flex-col md:flex-row">
+          <div className="relative h-[40vh] w-full md:w-1/2 overflow-hidden">
+            <Image
+              src={"/assets/trainer.jpg"}
+              fill
+              alt="Trainer image"
+              className="object-cover object-left-top"
+            />
+          </div>
+          <div className="w-full md:w-1/2">
+            <h3 className="w-1/2 border-b-2 border-mag md:text-right text-3xl p-2 px-4">
+              Cosmin Ioneasa
+            </h3>
+            <div className="p-4">
+              Simplu spus, sunt un cetățean român, mândru de originile sale de
+              pe plaiuri ieșene. Răspund cu implicare și entuziasm la numele
+              complet de Ștefan Cosmin Ioneasa. Virusarea mea cu pasiunea pentru
+              actul cultural-artistic al dansului a avut loc undeva în jurul
+              vârstei de 5 ani, pe vremea când încă nu eram capabil să îmi asum
+              anumite dorințe bine conturate și conștientizate în adevăratul
+              sens al cuvântului. Asta pentru că la vârsta aceea, mintea îți
+              zboară mai mult la joacă decât la pasiuni care să necesite
+              devotament și muncă intensă.
+            </div>
+            <div className="w-full flex justify-end">
+              <Button className="bg-mag hover:bg-mag-hover">
+                Vezi mai mult
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TrainerComponent;
